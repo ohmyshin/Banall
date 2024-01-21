1f0582313eed86faf3ea7185bb09e9a06a2a0e09
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2024-01-21 15:44:49 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,4 +2,13 @@ from pyrogram import idle
 from .config import Config
 from . import bot
 
-bot.start()
+
+
+async def main():
+    await asyncio.gather(bot.start())
+
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop_policy().get_event_loop()
+    loop.run_until_complete(main())
